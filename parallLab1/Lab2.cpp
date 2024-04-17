7fa093ffd5affd3387d2e4578b3016e936acfa89
Airfix982 <25roter52@gmail.com>
2024-04-17 17:02:57 +0400

    add: multiplication of matrix with several count of threads, calculated confidence intervals, added README.md

--- parallLab1/Lab2.cpp
+++ parallLab1/Lab2.cpp
@@ -2,8 +2,10 @@
 #include<random>
 #include <fstream>
 #include <iomanip>
+#include <cmath>
 #include <string>
 #include<chrono>
+#include<numeric>
 #include <Eigen/Dense>
 #include <omp.h>
 
@@ -90,65 +92,70 @@ bool compareMatrices(double** matrix1, double** matrix2, Eigen::MatrixXd& eigenR
 
 int main()
 {
-	const int repetitions = 5;
+	const int repetitions = 10;
 	std::vector<double> averageDurations;
 	std::vector<long long> averageOperations;
+	const std::vector<int> threadCounts = { 1, 2, 4, 10, 20 };
 
-	std::ofstream outFile("./csv/performance_openMP.csv");
-	outFile << "MatrixSize, AverageDuration, AverageOperations\n";
+	std::ofstream outFile("./performance_openMP.csv");
+	outFile << "MatrixSize, ThreadCount, MeanDuration, ConfidenceIntervalLower, ConfidenceIntervalUpper\n";
 
-	for (int matrixSize = 50; matrixSize <= 801; matrixSize += 50)
+	for (int matrixSize = 50; matrixSize <= 800; matrixSize += 50)
 	{
 		long long totalDuration = 0;
-		long long totalOperations = 0;
 
-		for (int i = 0; i < repetitions; ++i)
+		for (int threads : threadCounts)
 		{
-			double** matrix1 = getMatrix(matrixSize, matrixSize, false);
-			double** matrix2 = getMatrix(matrixSize, matrixSize, false);
-			double** resultMatrix = getMatrix(matrixSize, matrixSize, true);
-
-			long long operationsCounter = 0;
-
-			auto start = std::chrono::high_resolution_clock::now();
-			mulMatrices(matrix1, matrix2, resultMatrix, operationsCounter, matrixSize);
-			auto stop = std::chrono::high_resolution_clock::now();
-
-			auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
-
-			totalDuration += duration;
-			totalOperations += operationsCounter;
-
-
-			Eigen::MatrixXd eigenResult = convertToEigen(resultMatrix, matrixSize, matrixSize);
-			if (i == 0)
+			std::vector<double> durations;
+			for (int i = 0; i < repetitions; ++i)
 			{
-				bool isCorrect = compareMatrices(matrix1, matrix2, eigenResult, matrixSize, matrixSize);
-				if (!isCorrect)
+				omp_set_num_threads(threads);
+
+				double** matrix1 = getMatrix(matrixSize, matrixSize, false);
+				double** matrix2 = getMatrix(matrixSize, matrixSize, false);
+				double** resultMatrix = getMatrix(matrixSize, matrixSize, true);
+
+				long long operationsCounter = 0;
+
+				auto start = std::chrono::high_resolution_clock::now();
+				mulMatrices(matrix1, matrix2, resultMatrix, operationsCounter, matrixSize);
+				auto stop = std::chrono::high_resolution_clock::now();
+
+				auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+
+				durations.push_back(duration);
+
+
+				Eigen::MatrixXd eigenResult = convertToEigen(resultMatrix, matrixSize, matrixSize);
+				if (i == 0)
 				{
-					std::cerr << "Matrix multiplication is incorrect!" << std::endl;
+					bool isCorrect = compareMatrices(matrix1, matrix2, eigenResult, matrixSize, matrixSize);
+					if (!isCorrect)
+					{
+						std::cerr << "Matrix multiplication is incorrect!" << std::endl;
+					}
 				}
+
+				for (int j = 0; j < matrixSize; j++)
+				{
+					delete[] matrix1[j];
+					delete[] matrix2[j];
+					delete[] resultMatrix[j];
+				}
+				delete[] matrix1;
+				delete[] matrix2;
+				delete[] resultMatrix;
 			}
 
-			for (int j = 0; j < matrixSize; j++)
-			{
-				delete[] matrix1[j];
-				delete[] matrix2[j];
-				delete[] resultMatrix[j];
-			}
-			delete[] matrix1;
-			delete[] matrix2;
-			delete[] resultMatrix;
+		double mean = std::accumulate(durations.begin(), durations.end(), 0.0) / durations.size();
+		double sq_sum = std::inner_product(durations.begin(), durations.end(), durations.begin(), 0.0);
+		double stdev = std::sqrt(sq_sum / durations.size() - mean * mean);
+		double errorMargin = 1.96 * stdev / std::sqrt(durations.size());
+
+		outFile << matrixSize << ", " << threads << ", " << mean << ", " << (mean - errorMargin) << ", " << (mean + errorMargin) << "\n";
+
+		std::cout << "multiplicated " << matrixSize << " threads " << threads << " mean " << mean << std::endl;
 		}
-
-		double averageDuration = static_cast<double>(totalDuration) / repetitions;
-		long long averageOperation = totalOperations / repetitions;
-
-		outFile << matrixSize << ", " << averageDuration << ", " << averageOperation << "\n";
-
-		averageDurations.push_back(averageDuration);
-		averageOperations.push_back(averageOperation);
-		std::cout << "multiplicated " << matrixSize << " operations " << totalOperations << std::endl;
 	}
 
 	outFile.close();
